9b3b4ac743b85c06ad5340aed294d94061cf212c
likeanoa <nnm.fabbricotti@ziggo.nl>
2025-09-12 13:04:06 +0200

    Moved misc variables to designated file, updated comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os, psutil, shutil, socket, rich, getpass, platform, distro, time, subprocess
 
-from system import cpu, ram
+from system import cpu, ram, misc
 
 # Storage Vars
 
@@ -18,18 +18,13 @@ disk_usgprct = round(disk_used / disk_total * 100, 1)
 usr = getpass.getuser()
 arch = platform.machine()
 
-# Distro Vars (Linux Only)
+# Distro Vars -- HAS BEEN MOVEDTO ./system/misc.py !
 
-dist = distro.name()
-
-# Uptime Vars
-
-boot_time = psutil.boot_time()
-uptime_seconds = time.time() - boot_time
-uptime_hrs = int(uptime_seconds // 3600)
-uptime_mns = int((uptime_seconds % 3600) // 60)
+# Uptime Vars -- HAS BEEN MOVED TO ./system/misc.py !
 
 # CPU vars and functions -- HAS BEEN MOVED TO ./system/cpu.py !
+
+# RAM vars -- HAS BEEN MOVED TO ./system/ram.py !
 
 # Package functions
 
@@ -46,10 +41,10 @@ flatpak_count = get_pkgs("flatpak list --app --columns=application")
 pip_count = get_pkgs("pip list", skip=2)
 
 print(f"\033[38;2;0;255;255m\n{usr}\033[0m@\033[38;2;0;255;255m{hostname}\033[0m\n----------------------")
-print(f"OS:       {dist} {arch}")
+print(f"OS:       {misc.dist} {arch}")
 print(f"Memory:   {ram.mem_used}/{ram.mem_total} GB ({ram.mem_usedprct}%)")
 print(f"Storage:  {disk_used}/{disk_total} GB ({disk_usgprct}%)")
-print(f"Uptime:   {uptime_hrs} Hours, {uptime_mns} Minutes")
+print(f"Uptime:   {misc.uptime_hrs} Hours, {misc.uptime_mns} Minutes")
 print(f"CPU:      {cpu.get_cpu()}")
 print(f"Packages: {pacman_count} (Pacman), {flatpak_count} (Flatpak), {pip_count} (PIP)")
 
