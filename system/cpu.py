784b45d6638452e4c2237e84e2a13087e68f87a3
likeanoa <nnm.fabbricotti@ziggo.nl>
2025-09-12 15:48:38 +0200

    First v1.1-alpha commit

--- system/cpu.py
+++ system/cpu.py
@@ -1,9 +1,15 @@
+import os
+
 def get_cpu():
-    try:
-        with open("/proc/cpuinfo") as f:
-            for line in f:
-                if "model name" in line:
-                    return line.strip().split(": ")[1]
-    except:
-        pass
-    return "Unknown CPU"
+
+    system = os.system
+
+    if system == "Linux":
+        try:
+           with open("/proc/cpuinfo") as f:
+              for line in f:
+                   if "model name" in line:
+                     return line.strip().split(": ")[1]
+        except:
+            pass
+        return "Unknown CPU"
