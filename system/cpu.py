d2b2e5743cf299230b2ff1b71d0e3fdeea01e77c
Noa Fabbricotti <nnm.fabbricotti@ziggo.nl>
2025-09-13 10:40:31 +0200

    Fixed Issue #12: WMI Module not avail. on Linux

--- system/cpu.py
+++ system/cpu.py
@@ -1,32 +1,42 @@
-import platform, wmi, subprocess
+import platform, subprocess
+    
+def get_win_cpu():
+    if platform.system != "Windows":
+        return []
+    
+    import wmi
+    try:
+        pc = wmi.WMI()
+        cpu_name = pc.Win32_Processor()[0].Name
+        return cpu_name
+    except:
+        pass
+    return "Unknown CPU"
+
+def get_linux_cpu():
+    try:
+        with open("/proc/cpuinfo") as f:
+            for line in f:
+                if "model name" in line:
+                    return line.strip().split(": ")[1]
+    except:
+        pass
+    return "Unknown CPU"
+
+def get_darwin_cpu():
+    try:
+        result = subprocess.check_output(['sysctl', '-n', 'machdep.cpu.brand_string'], text=True)
+        return result.strip()
+    except:
+        pass
+    return "Unknown CPU"
 
 def get_cpu():
-
-    system = platform.system
+    system = platform.system()
 
     if system == "Linux":
-        try:
-           with open("/proc/cpuinfo") as f:
-              for line in f:
-                   if "model name" in line:
-                     return line.strip().split(": ")[1]
-        except:
-            pass
-        return "Unknown CPU"
-    
+        return get_linux_cpu()
     elif system == "Windows":
-        try:
-            pc = wmi.WMI()
-            cpu_name = pc.Win32_Processor()[0].Name
-            return cpu_name
-        except:
-            pass
-        return "Unknown CPU"
-    
+        return get_win_cpu()
     elif system == "Darwin":
-        try:
-            result = subprocess.check_output(['sysctl', '-n', 'machdep.cpu.brand_string'], text=True)
-            return result.strip()
-        except:
-            pass
-        return "Unknown CPU"
+        return get_darwin_cpu()
